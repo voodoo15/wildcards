1319c619269bc01f5adaab1e3f04a725299c9338
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-04-23 01:04:27 -0400

    Ran devise install and  added action mailer default url to development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  #Defined local default url for action mailer
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
