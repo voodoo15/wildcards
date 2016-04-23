a493e0d418a8471c6a3ec328e90ffe881a70ed38
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-04-23 16:16:14 -0400

    Commented out profile build method for now (I'll fix it when I create users via admin), updated seeds with team data, added associations to team and profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,30 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Team.create!(
+            team_name:  "Autumn",
+            tier:  "3",
+            team_abbreviation:  "AMN"
+)
+
 User.create!(email: 'bubba_smith@gmail.com',
              password: '123456',
-             password_confirmation: '123456')
+             password_confirmation: '123456'
+)
 
-# Profile.create!(
-#             team_id: 1,
-#             team_number: 1,
-#             nickname: "Dude",
-#             first_name:  "Bubba",
-#             last_name:  "Smith",
-#             address:  "123 Main St",
-#             city:  "Toronto",
-#             province:  "ON",
-#             postal_code:  "A1B 2C3",
-#             role:  "administrator",
-#             phone_number:  "4161234567",
-#             throws:  "R",
-#             bats:  "R",
-#             user_id:  1
-# )
+Profile.create!(
+            team_id: 1,
+            team_number: 1,
+            nickname: "Dude",
+            first_name:  "Bubba",
+            last_name:  "Smith",
+            address:  "123 Main St",
+            city:  "Toronto",
+            province:  "ON",
+            postal_code:  "A1B 2C3",
+            role:  "administrator",
+            phone_number:  "4161234567",
+            throws:  "R",
+            bats:  "R",
+            user_id:  1
+)
